552bfb87d88c3d48882c4f6f014348d78510caf6
Kelechi Egbuta <kelechi.k.egbuta@gmail.com>
2024-06-26 19:44:43 +0100

    feat: handle avatar upload in signup controller

--- server/app/common/globals/helpers/utils.ts
+++ server/app/common/globals/helpers/utils.ts
@@ -12,4 +12,13 @@ export class Utils {
   static lowercase(str: string): string {
     return str.toLowerCase()
   }
+
+  static genRandomInt(intLen: number): number {
+    const chars = '0123456789'
+    let result = ''
+    const charLen = chars.length
+    for (let i = 0; i < intLen; i++)
+      result += chars.charAt(Math.floor(Math.random() * charLen))
+    return parseInt(result, 10)
+  }
 }
--- server/app/modules/auth/controllers/signup.controller.ts
+++ server/app/modules/auth/controllers/signup.controller.ts
@@ -1,17 +1,50 @@
 import { ObjectId } from 'mongodb'
 import { Request, Response } from 'express'
+import { UploadApiResponse } from 'cloudinary'
 
 import { signupSchema } from '@auth/schemas/signup'
 import { JoiValidator } from '@global/decorators/joi-validation'
 import { authService } from '@service/db/auth.service'
 import { BadRequestError } from '@global/helpers/error-handler'
-import { IAuthDocument } from '@auth/interfaces/auth.interface'
+import { IAuthDocument, ISignUpData } from '@auth/interfaces/auth.interface'
+import { Utils } from '@global/helpers/utils'
+import { uploads } from '@global/helpers/cloudinary-upload'
 
 export class Signup {
   @JoiValidator(signupSchema)
   public async create(req: Request, res: Response): Promise<void> {
     const { username, password, email, avatarColor, avatarImage } = req.body
-    const userExists: IAuthDocument = await authService.getUser(username, email)
-    if (userExists) throw new BadRequestError('User already exists')
+    const dupUser: IAuthDocument = await authService.getUser(username, email)
+    if (dupUser) throw new BadRequestError('User already exists')
+
+    const authObjectId: ObjectId =  new ObjectId()
+    const userObjectId: ObjectId =  new ObjectId()
+    const uId = `${Utils.genRandomInt(16)}`
+    const authData: IAuthDocument = Signup.prototype.signupData({
+      _id: authObjectId,
+      uId,
+      username,
+      email,
+      password,
+      avatarColor
+    })
+    const result: UploadApiResponse = await uploads(
+      avatarImage, `${userObjectId}`, true, true
+    ) as UploadApiResponse
+    if (!result?.public_id)
+      throw new BadRequestError('Error occurred during upload. Try again')
+  }
+
+  private signupData(data: ISignUpData): IAuthDocument {
+    const { _id, uId, username, email, password, avatarColor } = data
+    return {
+      _id,
+      uId,
+      username: Utils.capitalize(username),
+      email: Utils.lowercase(email),
+      password,
+      avatarColor,
+      createdAt: new Date()
+    } as IAuthDocument
   }
 }

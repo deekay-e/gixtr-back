b12f620484f6877af4f2feedb4f3a8f7d1569572
Kelechi Egbuta <kelechi.k.egbuta@gmail.com>
2024-07-04 16:04:07 +0100

    chore: add post interface, model and validation schema

---
+++ server/app/modules/posts/interfaces/post.interface.ts
@@ -0,0 +1,62 @@
+import { ObjectId } from 'mongodb'
+import mongoose, { Document } from 'mongoose'
+
+//import { IReactions } from '@reaction/interfaces/reaction.interface'
+
+interface IReactions {
+  like: number
+  love: number
+  haha: number
+  wow: number
+  sad: number
+  angry: number
+}
+
+export interface IPostDocument extends Document {
+  _id?: string | mongoose.Types.ObjectId
+  userId: string
+  username: string
+  email: string
+  avatarColor: string
+  profilePicture: string
+  post: string
+  bgColor: string
+  commentsCount: number
+  imgVersion?: string
+  imgId?: string
+  feelings?: string
+  gifUrl?: string
+  privacy?: string
+  reactions?: IReactions
+  createdAt?: Date
+}
+
+export interface IGetPostsQuery {
+  _id?: ObjectId | string
+  username?: string
+  imgId?: string
+  gifUrl?: string
+}
+
+export interface ISavePostToCache {
+  key: ObjectId | string
+  currentUserId: string
+  uId: string
+  createdPost: IPostDocument
+}
+
+export interface IPostJobData {
+  key?: string
+  value?: IPostDocument
+  keyOne?: string
+  keyTwo?: string
+}
+
+export interface IQueryComplete {
+  ok?: number
+  n?: number
+}
+
+export interface IQueryDeleted {
+  deletedCount?: number
+}

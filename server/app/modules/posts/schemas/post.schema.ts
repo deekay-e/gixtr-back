bb14bb53d661ed18a09c72f5a5eca8d84232e4cc
Kelechi Egbuta <kelechi.k.egbuta@gmail.com>
2024-07-05 07:16:03 +0100

    fix: change post property privacy to scope

--- server/app/modules/posts/schemas/post.schema.ts
+++ server/app/modules/posts/schemas/post.schema.ts
@@ -3,7 +3,7 @@ import Joi, { ObjectSchema } from 'joi'
 const postSchema: ObjectSchema = Joi.object().keys({
   post: Joi.string().optional().allow(null, ''),
   bgColor: Joi.string().optional().allow(null, ''),
-  privacy: Joi.string().optional().allow(null, ''),
+  scope: Joi.string().optional().allow(null, ''),
   feelings: Joi.string().optional().allow(null, ''),
   gifUrl: Joi.string().optional().allow(null, ''),
   profilePicture: Joi.string().optional().allow(null, ''),
@@ -19,7 +19,7 @@ const postWithImageSchema: ObjectSchema = Joi.object().keys({
   }),
   post: Joi.string().optional().allow(null, ''),
   bgColor: Joi.string().optional().allow(null, ''),
-  privacy: Joi.string().optional().allow(null, ''),
+  scope: Joi.string().optional().allow(null, ''),
   feelings: Joi.string().optional().allow(null, ''),
   gifUrl: Joi.string().optional().allow(null, ''),
   profilePicture: Joi.string().optional().allow(null, ''),

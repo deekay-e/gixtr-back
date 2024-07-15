9b90df6b66475bf9c7a1151ce2cdcd3e16368ecd
Kelechi Egbuta <kelechi.k.egbuta@gmail.com>
2024-07-15 11:44:19 +0100

    feat: add comment model, interface and schema

---
+++ server/app/modules/comments/interfaces/comment.interface.ts
@@ -0,0 +1,38 @@
+import { ObjectId } from 'mongodb'
+import { Document } from 'mongoose'
+
+import { IReactions } from '@reaction/interfaces/reaction.interface'
+
+export interface ICommentDocument extends Document {
+  _id?: string | ObjectId
+  username: string
+  avatarColor: string
+  postId: string
+  profilePicture: string
+  comment: string
+  reactions?: IReactions
+  createdAt?: Date
+  userTo?: string | ObjectId
+}
+
+export interface ICommentJob {
+  postId: string
+  userTo: string
+  userFrom: string
+  username: string
+  comment: ICommentDocument
+}
+
+export interface ICommentNameList {
+  count: number
+  names: string[]
+}
+
+export interface IQueryComment {
+  _id?: string | ObjectId
+  postId?: string | ObjectId
+}
+
+export interface IQuerySort {
+  createdAt?: number
+}

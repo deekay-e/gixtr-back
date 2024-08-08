3f31a108f016b42482fc90d838cda2993fd7f899
Kelechi Egbuta <kelechi.k.egbuta@gmail.com>
2024-08-08 08:46:39 +0100

    feat: handle user login maps via sockets

--- server/app/common/sockets/user.ts
+++ server/app/common/sockets/user.ts
@@ -1,8 +1,11 @@
 import { Server, Socket } from 'socket.io'
 
-import { ISocketData } from '@user/interfaces/user.interface'
+import { ILogin, ISocketData } from '@user/interfaces/user.interface'
 
 export let socketIOUserObject: Server
+export const connectedUsersMap: Map<string, string> = new Map()
+
+const users: string[] = []
 
 export class SocketIOUserHandler {
   private io: Server
@@ -14,6 +17,10 @@ export class SocketIOUserHandler {
 
   public listen(): void {
     this.io.on('connection', (socket: Socket) => {
+      socket.on('setup', (data: ILogin) => {
+        this.addClientToMap(data.userId, socket.id)
+      })
+
       socket.on('block user', (data: ISocketData) => {
         this.io.emit('blocked user id', data)
       })
@@ -21,6 +28,26 @@ export class SocketIOUserHandler {
       socket.on('unblock user', (data: ISocketData) => {
         this.io.emit('unblocked user id', data)
       })
+
+      socket.on('disconnect', () => {
+        this.removeClientFromMap(socket.id)
+      })
     })
+  }
+
+  private addClientToMap(userId: string, socketId: string): void {
+    if (!connectedUsersMap.has(userId)) connectedUsersMap.set(userId, socketId)
+  }
+
+  private removeClientFromMap(socketId: string): void {
+    if (Array.from(connectedUsersMap.values()).includes(socketId)) {
+      const disconnectedUser: [string, string] = [...connectedUsersMap].find(
+        (user: [string, string]) => {
+          return user[1] === socketId
+        }
+      ) as [string, string]
+      connectedUsersMap.delete(disconnectedUser[0])
+      // send event to the client
+    }
   }
 }

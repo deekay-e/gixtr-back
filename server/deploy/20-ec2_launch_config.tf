5e9100f503226ecb49444b4d71e797a948140fbc
Kelechi Egbuta <kelechi.k.egbuta@gmail.com>
2024-08-23 10:02:58 +0100

    feat: add script to execute on asg launch

--- server/deploy/20-ec2_launch_config.tf
+++ server/deploy/20-ec2_launch_config.tf
@@ -6,6 +6,7 @@ resource "aws_launch_configuration" "asg_launch_config" {
   associate_public_ip_address = false
   iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
   security_groups = [aws_security_group.autoscaling_group_sg.id]
+  user_data = filebase64("${path.module}/scripts/setup.sh")
 
   lifecycle {
     create_before_destroy = true
---
+++ server/deploy/scripts/setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+function is_installed {
+  local return_=1
+
+  type $1 >/dev/null 2>&1 || { local return_=0; }
+  echo "$return_"
+}
+
+sudo yum update -y
+
+# install curl if it doesn't exist
+if [ $(is_installed curl) == 0 ]; then
+  sudo yum install -y curl
+fi
+
+# install node if it doesn't exist
+if [ $(is_installed node) == 0 ]; then
+  curl -fsSL https://rpm.nodesource.com/setup_lts.x | sudo bash -
+  sudo yum install -y nodejs
+fi
+
+# install git if it doesn't exist
+if [ $(is_installed git) == 0 ]; then
+  sudo yum install -y git
+fi
+
+# install docker if it doesn't exist
+if [ $(is_installed docker) == 0 ]; then
+  sudo amazon-linux-extras install -y docker
+  sudo systemctl start docker
+  sudo docker run --name gixtr-redis -p 6379:6379 --restart always --detach redis
+fi
+
+# install pm2 if it doesn't exist
+if [ $(is_installed pm2) == 0 ]; then
+  npm install -g pm2
+fi
+
+cd /home/ec2-user
+
+git clone -b dev https://github.com/deekay-e/gixtr-back.git
+cd gixtr-back
+npm install
+aws s3 sync s3://gixtr-env/dev .
+unzip env-file.zip
+cp .env.production .env
+npm run build
+npm run start
